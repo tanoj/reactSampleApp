d2e801729e1f706d2e318f292c496443a5aa960c
routt <tanuj.kumar.rout@mavenir.com>
2021-05-09 14:04:21 +0530

    component added

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React  from 'react';
+
+const Footer = function(){
+    return(
+        <h2> Welcome to Jajpur Town</h2>
+    )
+
+}
+
+export default Footer;
---
+++ my-app/src/components/Header1.js
@@ -0,0 +1,10 @@
+import React  from 'react';
+
+const header = function(){
+    return(
+        <h2> Welcome to Jajpur Town</h2>
+    )
+
+}
+
+export default header;
